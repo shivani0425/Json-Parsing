ec628c7829b76676b2dbf74b33ff3610a8dcc436
shivani0425 <102775655+shivani0425@users.noreply.github.com>
2022-05-04 11:47:34 +0530

    Add files via upload
    In this Demo i parsed nested JSON.

---
+++ URLSessionPractice/URLSessionPractice/Model/EmployeeModel.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct EmployeeModel {
+    let id: String
+    let name: String   
+    let street: String
+    let zipcode: String
+    let lat: String
+    let lng: String
+
+}
